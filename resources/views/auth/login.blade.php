0c43d0ff4821bd60d23d0eafa4ff3ab285087224
yuda <yudaputrau@gmail.com>
2018-10-18 21:13:35 +0700

    auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,52 +1,63 @@
-@extends('admin-lte::layouts.auth')
+@extends('layouts.guest')
+
+@section('title', 'Login')
 
 @section('content')
-<div class="login-box-body">
-  <p class="login-box-msg">Sign in to start your session</p>
-
-  <form action="{{ route('login') }}" method="POST">
-    {{ csrf_field() }}
-    <div class="form-group has-feedback{{ $errors->has('email') ? ' has-error' : '' }}">
-      <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email" required autofocus>
-      <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-      @if ($errors->has('email'))
-      <span class="help-block">{{ $errors->first('email') }}</span>
-      @endif
-    </div>
-    <div class="form-group has-feedback{{ $errors->has('password') ? ' has-error' : '' }}">
-      <input id="password" type="password" class="form-control" name="password" placeholder="Password" required>
-      <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-      @if ($errors->has('password'))
-      <span class="help-block">{{ $errors->first('password') }}</span>
-      @endif
-    </div>
+<div class="login-page">
     <div class="row">
-      <div class="col-xs-8">
-        <div class="checkbox icheck">
-          <label>
-            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }} value="1"> Remember Me
-          </label>
+        {{-- <div class="col-md-4 col-sm-6 col-md-offset-4 col-sm-offset-3"> --}}
+        <div class="login-form">
+            <p class="text-center lead">
+                Selamat datang di aplikasi <br><strong>{{ config('app.name') }}</strong>
+            </p>
+            <p class="text-center">Silakan login untuk melanjutkan aktifitas Anda.</p>
+            <div class="panel panel-default">
+                <div class="panel-heading"><h3 class="panel-title text-center">Login</strong></h3></div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
+                            <label for="username" class="col-md-4 control-label">Username</label>
+
+                            <div class="col-md-8">
+                                <input id="username" type="text" class="form-control" name="username" value="{{ old('username') }}" required autofocus>
+                                @if ($errors->has('username'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('username') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-8">
+                                <input id="password" type="password" class="form-control" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-8 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary btn-block">
+                                    Login
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-      </div>
-      <!-- /.col -->
-      <div class="col-xs-4">
-        <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-      </div>
-      <!-- /.col -->
     </div>
-  </form>
-
-  {{-- <div class="social-auth-links text-center">
-    <p>- OR -</p>
-    <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
-    Facebook</a>
-    <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
-    Google+</a>
-  </div>
-  <!-- /.social-auth-links --> --}}
-
-  <a href="{{ route('password.request') }}">I forgot my password</a><br>
-  <a href="{{ route('register') }}" class="text-center">Register a new membership</a>
-
 </div>
+@endsection
+@section('ext_css')
+{{ Html::style('css/login.css') }}
 @endsection
